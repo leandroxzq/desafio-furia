6fac2a9b3841ae9ffdd15bcb604fa33d41adff9e
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-05-01 19:37:59 -0300

    env local

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCc9B5mgYYE3kikjM9GbEKkZHoJccVWCsc",
-	authDomain: "furia-chat-8aaa0.firebaseapp.com",
-	projectId: "furia-chat-8aaa0",
-	storageBucket: "furia-chat-8aaa0.firebasestorage.app",
-	messagingSenderId: "368910830864",
-	appId: "1:368910830864:web:f6775eea211400597ba995",
-	measurementId: "G-T5HY1S00YK",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
